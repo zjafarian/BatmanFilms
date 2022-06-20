fad14c38f648ef5e68ee4f0b516b9410fea4bd17
zjafarian <zjafarian88@gmail.com>
2022-06-20 17:16:29 +0430

    complete singleActivity design in SingleFragmentActivity.kt

--- app/src/main/java/com/example/batmanfilms/view/activity/SingleFragmentActivity.kt
+++ app/src/main/java/com/example/batmanfilms/view/activity/SingleFragmentActivity.kt
@@ -2,11 +2,49 @@ package com.example.batmanfilms.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.batmanfilms.R
+import com.example.batmanfilms.databinding.ActivitySingleFragmentBinding
 
-class SingleFragmentActivity : AppCompatActivity() {
+abstract class SingleFragmentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySingleFragmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_fragment)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_single_fragment)
+
+        val fragmentManager = supportFragmentManager
+        val fragment = fragmentManager.findFragmentById(R.id.fragment_container)
+
+        //check fragment is null or not
+        if (fragment == null) {
+
+            //if fragment is null add fragment by fragmentManager and beginTransaction
+            createFragment()?.let {
+                fragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, it, FRAGMENT_TAG)
+                    .commit()
+            }
+
+        } else {
+            //if fragment isn't null, replace new fragment instead old fragment by fragmentManager and beginTransaction
+            createFragment()?.let {
+                fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, it, FRAGMENT_TAG)
+                    .commit()
+            }
+
+        }
+
+    }
+
+
+    abstract fun createFragment(): Fragment?
+
+    companion object {
+        const val FRAGMENT_TAG = "FragmentActivity"
     }
 }
