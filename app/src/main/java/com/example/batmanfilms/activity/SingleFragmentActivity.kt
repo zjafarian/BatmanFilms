dc215461d118d60115d2c6f87190c8b57f04b16d
zjafarian <zjafarian88@gmail.com>
2022-06-20 16:53:51 +0430

    add dependencies to gradle

--- app/src/main/java/com/example/batmanfilms/activity/SingleFragmentActivity.kt
+++ app/src/main/java/com/example/batmanfilms/activity/SingleFragmentActivity.kt
@@ -7,6 +7,6 @@ import com.example.batmanfilms.R
 class SingleFragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single_fragment)
     }
 }
