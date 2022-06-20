0498ac46f776770f7c5013594bf24c4c2a2db2f8
zjafarian <zjafarian88@gmail.com>
2022-06-20 17:10:56 +0430

    create BatmanMainActivity.kt and change launcher activity in AndroidManifest.xml

---
+++ app/src/main/java/com/example/batmanfilms/view/activity/BatmanMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.batmanfilms.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BatmanMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batman_main)
+    }
+}
